37d5b31e45f7df0b210ed7aaeabe8a5a89aa3d1d
Abdías Neftalí Estrada Roca <abdias32@gmail.com>
2020-06-26 11:50:07 -0600

    03_Layout_en_gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { Layout, SEO, Jumbo } from "../components"
+import { SEO, Jumbo } from "../components"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <Jumbo />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/gracias/">Go to gracias</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
