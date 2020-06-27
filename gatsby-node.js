92d7bcc543ef89d9283b78b62d47bf567b548a32
Abdías Neftalí Estrada Roca <abdias32@gmail.com>
2020-06-26 20:47:22 -0600

    09_Paginas_en_funcion_de_los_datos

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,40 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const productTemplate = path.resolve(`src/templates/Product.js`)
+  const result = await graphql(`
+    query GET_SKUS {
+      allStripeSku: allStripePrice {
+        edges {
+          node {
+            id
+            price: unit_amount
+            product {
+              name
+              metadata {
+                description
+                img
+                wear
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  result.data.allStripeSku.edges.forEach(({ node }) => {
+    createPage({
+      path: `${node.id}`,
+      component: productTemplate,
+    })
+  })
+}
