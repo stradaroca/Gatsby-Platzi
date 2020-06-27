92221690cbbc2304587e52c1f34ada5cd097b256
Abdías Neftalí Estrada Roca <abdias32@gmail.com>
2020-06-26 18:58:38 -0600

    08_Productos_en_react

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { SEO, Jumbo } from "../components"
-import styled from "styled-components"
+import { SEO, Jumbo, Products } from "../components"
 
 export const query = graphql`
-  query GET_DESCRIPTION {
+  query GET_DATA {
     allSite {
       edges {
         node {
@@ -14,17 +13,22 @@ export const query = graphql`
         }
       }
     }
-  }
-`
-
-const Button = styled.button`
-  width: 8rem;
-  background-color: #98ca3f;
-  border: none;
-  border-radius: 10px;
-  color: ${props => props.color};
-  &:hover {
-    transform: scale(1.4);
+    allStripeSku: allStripePrice {
+      edges {
+        node {
+          id
+          price: unit_amount
+          product {
+            name
+            metadata {
+              description
+              img
+              wear
+            }
+          }
+        }
+      }
+    }
   }
 `
 
@@ -32,7 +36,7 @@ const IndexPage = ({ data }) => (
   <>
     <SEO title="Home" />
     <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <Button color="gray">Comprar</Button>
+    <Products products={data.allStripeSku.edges} />
   </>
 )
 
